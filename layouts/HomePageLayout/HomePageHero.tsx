67ea77827492b834b882ab9245e2de3867f9e813
Umang Patel <umang.patel@augustinfotechteam.com>
2020-09-17 14:03:00 +0530

    added heroku-postbuild script to packege.json file

--- layouts/HomePageLayout/HomePageHero.tsx
+++ layouts/HomePageLayout/HomePageHero.tsx
@@ -4,6 +4,38 @@ import { Box, Grid, Typography } from '@material-ui/core';
 import useTranslation from 'next-translate/useTranslation';
 import FreeTrialButton from 'components/FreeTrialButton';
 
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Icon from '@material-ui/core/Icon';
+
+function styles(theme) {
+  return ({
+    root: {
+      margin: 0,
+      padding: theme.spacing(2),
+    },
+  });
+}
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
 const useStyles = makeStyles((theme) => ({
   section: {
     alignItems: 'center',
@@ -42,6 +74,15 @@ const useStyles = makeStyles((theme) => ({
 function HomePageHero(): ReactElement {
   const classes = useStyles();
   const { t } = useTranslation();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <section className={classes.section}>
@@ -78,6 +119,27 @@ function HomePageHero(): ReactElement {
           >
             <FreeTrialButton className={classes.freeTrialCta} />
           </Box>
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            mt={4}
+          >
+            <Button onClick={handleClickOpen} className="playButton">
+              <Icon>play_circle_outline</Icon>
+            </Button>
+          </Box>
+          <Dialog className="videoPopup" fullScreen onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+            <IconButton aria-label="close" className="closeButton" onClick={handleClose}>
+                <CloseIcon />
+            </IconButton>
+            <DialogContent>
+                <video autoPlay className="video" controls>
+                  <source src='/banner-video.mp4' type="video/mp4" />
+                  Your browser does not support the video tag.
+              </video>
+            </DialogContent>
+          </Dialog>
         </Grid>
       </Grid>
     </section>
